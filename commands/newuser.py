60b9b70d8095454a854402b0bb0d99f9bf5af57b
Francesco Masala <francescomasala270202@gmail.com>
2019-07-11 23:13:28 +0200

    Bug Fix: Log output

--- commands/newuser.py
+++ commands/newuser.py
@@ -11,13 +11,14 @@ from datetime import datetime
 
 def newuser(bot, update):
     for new in update.message.new_chat_members:
-        update.message.reply_text('Benvenuto {username} in {chat_title}'
+        update.message.reply_text('Benvenuto {username} in {gruppo}'
                                   .format(username=update.message.from_user.first_name,
-                                          chat_title=update.message.chat.title))
+                                          gruppo=update.message.chat.title))
 
 
 def newuser_handler(bot, update):
     newuser(bot, update)
     messagetime = datetime.strftime(datetime.today(), '%H:%M del %d/%m/%Y')
-    print('User: {} con ID: {} '.format(user['username'], user['id'])
-          + "Ha appena eseguito il seguente comando: /server alle ore " + messagetime)
+    print('Il seguente utente {username} e appena entrato nel gruppo {gruppo}'
+          .format(username=update.message.from_user.first_name,
+                  gruppo=update.message.chat.title))  # todo accento
