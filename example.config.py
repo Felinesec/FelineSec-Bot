f7fe145efaf046d74028722bc80ad0c9d21dc4fb
Masala Francesco <francescomasala270202@gmail.com>
2019-07-17 21:34:38 +0200

    Update example.config.py

--- example.config.py
+++ example.config.py
@@ -6,5 +6,5 @@
 #   Created by Francesco Masala & Jack Rendor
 #   Mozilla Public License
 #
-bot_token = "INSERT_TOKEN_HERE"
-admin_list = [1234567, 12345678]
+bot_token = "INSERT_TOKEN_HERE" # Token del bot
+admin_list = [1234567, 12345678] # Lista utenti "Sudoers"
