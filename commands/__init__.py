61181d60615d1d198b163f98d1416596cf882f18
Talebian12 <nicco199os@7gmail.com>
2019-07-19 22:47:40 +0000

    minor fixes

--- commands/__init__.py
+++ commands/__init__.py
@@ -6,6 +6,6 @@
 #   Created by Francesco Masala & Jack Rendor
 #   Mozilla Public License
 #
-__all__ = ["help", "start" ,"server" ,"newuser", "definisci", "setrules"]
+__all__ = ["build_menu", "help", "start" ,"server" ,"newuser", "definisci", "setrules"]
 
 from commands import *
