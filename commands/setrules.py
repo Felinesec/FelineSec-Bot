a11650e000acf2a1fb86669c57c1534cbfa6e401
francescomasala <francescomasala270202@gmail.com>
2019-07-20 23:59:19 +0200

    Added sudoers users to setrules.py & MD error message

--- commands/setrules.py
+++ commands/setrules.py
@@ -9,9 +9,22 @@
 
 import json
 from datetime import datetime
+import config
 
 
 def setrules_handler(bot, update):
+    user = update.message.from_user
+    messagetime = datetime.strftime(datetime.today(), '%H:%M del %d/%m/%Y')
+    if not update.message.from_user.id in config.admin_list:
+        sudoers=("User not in the sudoers list.")
+        sudoers_markdown = "`" + sudoers + "`"
+        update.message.reply_markdown(sudoers_markdown)
+        print('User: {} con ID: {} '.format(user['username'], user['id'])
+              + "Ha provato ed eseguire il comando /setrules alle ore: " + messagetime
+              + "\n La sua richiesta \u00e8 stata rifiutata poich\u00e8 esso non rientra nella lista sudoers"
+              )
+        return
+
     new_rules = "{}".format(update.message.text[10:])
 
     rules = {"rules": new_rules }
@@ -22,6 +35,5 @@ def setrules_handler(bot, update):
 
     # LOG del bot
     user = update.message.from_user
-    messagetime = datetime.strftime(datetime.today(), '%H:%M del %d/%m/%Y')
     print('User: {} con ID: {} '.format(user['username'], user['id'])
           + "Ha appena eseguito il seguente comando: /setrules alle ore " + messagetime)
