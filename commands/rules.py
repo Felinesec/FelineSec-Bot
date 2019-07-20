2914f7129bf30f8da7fe3038373d5b5ebfeff0e8
francescomasala <francescomasala270202@gmail.com>
2019-07-20 11:24:50 +0200

    Fixed trailing whitespace

--- commands/rules.py
+++ commands/rules.py
@@ -10,6 +10,7 @@
 import json
 from datetime import datetime
 
+
 def rules_handler(bot, update):    
     with open('commands/felinesec.rules.json') as rules_js:
         data = json.load(rules_js)
