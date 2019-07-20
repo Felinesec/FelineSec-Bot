67519d7046c1a7c6173792d6a6de700b488e789c
Talebian <nicco199os@gmail.com>
2019-07-20 12:39:14 +0200

    Update help.py

--- commands/help.py
+++ commands/help.py
@@ -10,8 +10,14 @@ from datetime import datetime
 
 
 def help_handler(bot, update):
-    text="/server = Indica le principali statistiche del server (Uptime, utilizzo della cpu e della ram)"
-    update.message.reply_text(text)
+    text={
+        "*/server*: Indica le principali statistiche del server _(Uptime, utilizzo della cpu e della ram)_\n"
+        "*/wiki {arg}*: cerca la definizione su wikipedia\n"
+        "*/setrules {arg}*: imposta le regole\n"
+        "*/rules {arg}*:vedi le regole\n"
+        "*/help*: mostra questa guida"
+    }
+    update.message.reply_markdown(text)
     user = update.message.from_user
     messagetime = datetime.strftime(datetime.today(), '%H:%M %d/%m/%Y')
     print('User: {} con ID: {} '.format(user['username'], user[
