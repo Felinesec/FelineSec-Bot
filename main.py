a105c7d8dbca453acaa1785359bceb7ba78a1579
Talebian <nicco199os@gmail.com>
2019-07-20 11:44:14 +0200

    Update main.py
    fix

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def main():
     updh(CommandHandler('start', start.start_handler))
     updh(CommandHandler('server', server.server_handler))
     updh(CommandHandler('help', help.help_handler))
-    dpdh(CommandHandler('rules', rules.rules_handler))
+    updh(CommandHandler('rules', rules.rules_handler))
     updh(CommandHandler('wiki', definisci.definisci_handler))
     updh(CommandHandler('setrules', setrules.setrules_handler))
     dp.add_handler(MessageHandler(None, newuser.newuser_handler))
