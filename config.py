c761644ad86aa266e3bf798dd08d265a57c2cf9a
Masala Francesco <francescomasala270202@gmail.com>
2019-07-24 17:14:49 +0200

    Create config.py
    This is only an example telegram token used for TravisCI

---
+++ config.py
@@ -0,0 +1,11 @@
+# This is only an example telegram token used for TravisCI
+#!/usr/bin/env python3
+#   config.py
+#   Python 3.7
+#   Version 0.1
+#
+#   Created by Francesco Masala & Jack Rendor
+#   Mozilla Public License
+#
+bot_token = "892420730:AAEMHseKT8BEQAtLDirPNyImyJ7GuAKiIwU"
+admin_list = [44222507, 33003926, 358176315]
