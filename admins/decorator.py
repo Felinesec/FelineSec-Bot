a709aa770a352ab50bfc6b03e1e479ccc01c771b
Hersel Giannella <hersel1991@gmail.com>
2019-08-24 22:59:36 +0200

    aggiunto decorator.admin_id
    Questo decorator ha la capacità di determinare se un utente è admin senza inserire manualmente l'ID

--- admins/decorator.py
+++ admins/decorator.py
@@ -56,3 +56,52 @@ def private(fn):
     else:
       return False
   return wrapper
+#CLASSE MWT PER IL DECORATOR CHE TROVA AUTOMATICAMENTE ADMIN
+class MWT(object):
+    """Memoize With Timeout"""
+    _caches = {}
+    _timeouts = {}
+
+    def __init__(self,timeout=2):
+        self.timeout = timeout
+
+    def collect(self):
+        """Clear cache of results which have timed out"""
+        for func in self._caches:
+            cache = {}
+            for key in self._caches[func]:
+                if (time.time() - self._caches[func][key][1]) < self._timeouts[func]:
+                    cache[key] = self._caches[func][key]
+            self._caches[func] = cache
+
+    def __call__(self, f):
+        self.cache = self._caches[f] = {}
+        self._timeouts[f] = self.timeout
+
+        def func(*args, **kwargs):
+            kw = sorted(kwargs.items())
+            key = (args, tuple(kw))
+            try:
+                v = self.cache[key]
+                print("cache")
+                if (time.time() - v[1]) > self.timeout:
+                    raise KeyError
+            except KeyError:
+                print("new")
+                v = self.cache[key] = f(*args,**kwargs),time.time()
+            return v[0]
+        func.func_name = f.__name__
+
+        return func
+#CONTROLLER ADMIN
+@MWT(timeout=10*10)
+def get_admin_ids(bot, chat_id):
+    """Returns a list of admin IDs for a given chat. Results are cached for 1 hour."""
+    return [admin.user.id for admin in bot.get_chat_administrators(chat_id)]
+#DECORATOR ADMIN
+def admin_id(func):
+    @wraps(func)
+    def wrapped(bot, update):
+      if update.message.from_user.id in get_admin_ids(bot, update.message.chat_id):
+        return func(bot, update)
+    return wrapped
